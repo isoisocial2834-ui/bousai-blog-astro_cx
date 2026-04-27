ac94482061e12d1001fca2ef879eda1ffbe92a42
isoisocial2834-ui <isoisocial2834@gmail.com>
2026-04-27 22:49:27 +0900

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel";
 
 export default defineConfig({
   site: "http://localhost:4321/",
